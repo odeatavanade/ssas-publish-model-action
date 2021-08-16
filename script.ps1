2b61aab1f2131900e0c6abe9cf1861cfdd3a2979
Olivier Delmotte (Avanade) <70886462+odeatavanade@users.noreply.github.com>
2021-08-17 01:26:57 +0200

    Deploy package ?

--- script.ps1
+++ script.ps1
@@ -23,17 +23,14 @@ $sp.tenantId
 
 $secureSecret = ConvertTo-SecureString -String $sp.clientSecret -AsPlainText -Force
 $creds = New-Object PSCredential @($sp.clientId, $secureSecret)
+$environment = $AnalysisInstance.Split('/')[2];
+Add-AzureAnalysisServicesAccount -Credential $creds -ServicePrincipal -TenantId $sp.tenantId -RolloutEnvironment $environment
 
 $model = Get-Content $PackagePath -Encoding UTF8 | ConvertFrom-Json
-
 $tmsl = '{"createOrReplace":{"object":{"database":"existingModel"},"database":{"name":"emptyModel"}}}' | ConvertFrom-Json
 $tmsl.createOrReplace.object.database = $ModelName
 $tmsl.createOrReplace.database = $Model
 $tmsl = ConvertTo-Json $tmsl -Depth 100 -Compress
-
-
-$environment = $AnalysisInstance.Split('/')[2];
-$result = Add-AzureAnalysisServicesAccount -Credential $creds -ServicePrincipal -TenantId $sp.tenantId -RolloutEnvironment $environment
 
 Invoke-ASCmd -Server $Server -Query $tmsl
 # Invoke-ASCmd -Server $AnalysisInstance -Database $ModelName -ApplicationId $sp.clientId -ServicePrincipal -TenantID $sp.tenantId -Credential $creds
