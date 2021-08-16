2fe55f3f3144f683307ce4d030e55cc594f01c52
Olivier Delmotte (Avanade) <70886462+odeatavanade@users.noreply.github.com>
2021-08-17 00:11:12 +0200

    Add module + fix manifest

--- script.ps1
+++ script.ps1
@@ -1,1 +1,11 @@
+[CmdletBinding()]
+param(
+    [string] $PackagePath,
+    [string] $AnalysisInstance
+)
+
 Write-Host "Hello from my action"
+
+Import-Module (Join-Path $PSScriptRoot "SQLServer")
+
+Write-Host "Will import $PackagePath to $AnalysisInstance"
